b03ee7608692af52a5c84de86a6dbbf27cf92a54
mzuleta <mzuleta@dc.uba.ar>
2021-07-07 14:28:18 -0300

    First commit

---
+++ api/controllers/TransactionController.js
@@ -0,0 +1,76 @@
+var transaction = require("../models").Transaction;
+
+module.exports = {
+   async listDesc(req, res){
+       try {
+           const allTrans = await transaction.findAll({
+               order: [["createdAt", "DESC"]]
+           });
+           return res.status(200).send(allTrans);
+       } catch (err){
+           console.log(err.message);
+           return res.status(400).send(err);
+       }
+   },
+   async create(req, res){
+       try {
+           if(!req.body.concept || !req.body.amount || !req.body.date || !req.body.type){
+               res.status(400).send('Please complete all required fields');
+           }
+           const newTransaction = transaction.create(req.body);
+           return res.status(201).send(newTransaction);
+       } catch(err){
+           console.log(err.message);
+           return res.status(400).send(err);
+       }
+   },
+   async update(req, res){
+       try {
+           const transToUpdate = await transaction.findByPk(req.params.id);
+           if(transToUpdate){
+               const updated = await transToUpdate.update(req.body);
+               return res.status(200).send(updated);
+           }
+       } catch (err){
+        console.log(err.message);
+        return res.status(400).send(err);
+       }
+   },
+   async listIncome(req, res){
+       try {
+           const inTrans = await transaction.findAll({
+               where : {type: 'IN'}
+           });
+           return res.status(200).send(inTrans);
+
+       } catch(err){
+           console.log(err.message);
+           return res.status(400).send(err);
+       }
+   },
+   async listOutcome(req, res){
+    try {
+        const outTrans = await transaction.findAll({
+            where : {type: 'OUT'}
+        });
+        return res.status(200).send(outTrans);
+
+    } catch(err){
+        console.log(err.message);
+        return res.status(400).send(err);
+    }
+},
+    async destroy(req, res){
+        try{
+            const transToDestroy = await transaction.findByPk(req.params.id);
+            if(transToDestroy){
+                const deletedTrans = await transToDestroy.destroy();
+                return res.status(204).send(deletedTrans);
+            }
+            return null;
+        } catch(err){
+            console.log(err.message);
+            return res.status(400).send(err);
+        }
+    }
+};
---
+++ api/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var transactionController = require('../controllers/TransactionController');
+
+/* GET home page. */
+router.get('/', transactionController.listDesc);
+router.post('/', transactionController.create);
+router.get('/in', transactionController.listIncome);
+router.get('/out', transactionController.listOutcome);
+router.delete('/:id', transactionController.destroy);
+router.put('/:id', transactionController.update);
+
+module.exports = router;
