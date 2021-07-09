08aac518e48d3a89b1dbdb5df48b735fc7f9df2b
unknown <martinzuleta@hotmail.com>
2021-07-09 16:02:58 -0300

    Starting front end

---
+++ client/src/components/InputTransaction.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const InputTransaction = () => {
+  return (
+    <>
+      <h1>Budget Manager</h1>
+      <form>
+          <input type="text" />
+          <button>Add Transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default InputTransaction;
