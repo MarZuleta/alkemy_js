50380127296a22ed529ccbb57506310b2b3f67f2
unknown <martinzuleta@hotmail.com>
2021-08-23 15:52:51 -0300

    Center edit modal

--- client/src/components/EditTransaction.js
+++ client/src/components/EditTransaction.js
@@ -1,10 +1,28 @@
 import React, {useState} from 'react';
 import { Button, Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function SimpleModal() {
     // getModalStyle is not a pure function, we roll the style only on the first render
     const [open, setOpen] = useState(false);
-  
+    const classes = useStyles();
+
+    const modalStyle = {
+      top: '50%',
+      left: '50%',
+      transform: `translate(-50%, -50%)`
+    };
     const handleOpen = () => {
       setOpen(true);
     };
@@ -14,7 +32,7 @@ function SimpleModal() {
     };
   
     const body = (
-      <div >
+      <div style={modalStyle} className={classes.paper}>
         <h2 id="simple-modal-title">Text in a modal</h2>
         <p id="simple-modal-description">
           Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
